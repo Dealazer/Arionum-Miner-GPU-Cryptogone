ea2ef4dab342cffb6b574462fea4549210ffd35d
Guli <vert.guli@gmail.com>
2018-02-02 02:08:32 +0100

    OK

--- src/common/miner.cpp
+++ src/common/miner.cpp
@@ -130,6 +130,8 @@ void Miner::checkArgon(string *base, string *argon, string *nonce) {
     result.set_str(duration, 10);
     mpz_tdiv_q(rest.get_mpz_t(), result.get_mpz_t(), diff.get_mpz_t());
 
+    gmp_printf("Submitting - %Zd\n", rest.get_mpz_t());
+
     if (mpz_cmp(rest.get_mpz_t(), ZERO.get_mpz_t()) > 0 && mpz_cmp(rest.get_mpz_t(), limit.get_mpz_t()) <= 0) {
         mpz_cmp(rest.get_mpz_t(), BLOCK_LIMIT.get_mpz_t()) < 0 ? stats->newBlock() : stats->newShare();
         gmp_printf("Submitting - %Zd - %s - %s\n", rest.get_mpz_t(), nonce->data(), argon->data());
