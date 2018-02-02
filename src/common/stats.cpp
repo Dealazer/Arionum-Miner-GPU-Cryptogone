a41db3d6568cbe07283f5b03f5ac933623358eb3
Guli <vert.guli@gmail.com>
2018-02-02 02:55:40 +0100

    OK

--- src/common/stats.cpp
+++ src/common/stats.cpp
@@ -71,8 +71,6 @@ void Stats::newRejection() {
 
 void Stats::newDl(long dl) {
     long prev = bestDl;
-    cout << dl << endl;
-    cout << prev << endl;
     if (dl > 0 && dl < prev)
         bestDl.compare_exchange_weak(prev, dl);
 }
