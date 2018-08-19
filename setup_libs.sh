49157408fb59ff7f3f80278801fb46f62437aa0b
cryptogone <cryptogone.dev@gmail.com>
2018-08-19 20:37:39 +0200

    unique script to setup libs

--- setup_libs.sh
+++ setup_libs.sh
@@ -6,8 +6,14 @@ if [ "$1" = "vs2015" ]; then
   vcver="vc14"
 elif [ "$1" = "vs2017" ]; then
   vcver="vc15"
+elif [ "$1" = "linux" ]; then
+  # maybe too much things here...
+  sudo apt install gcc-6 g++-6 libgmp-dev python-dev libboost-dev libcpprest-dev cmake git make zlib1g-dev libssl-dev libargon2-0-dev -y
+  # fix for cpprest not finding xlocale
+  sudo ln -s /usr/include/locale.h /usr/include/xlocale.h
+  exit 0
 else
-  echo "Usage: setup_libs vs2015 / setup_libs vs2017"
+  echo "Usage: setup_libs vs2015 / setup_libs vs2017 / setup_libs linux"
   exit 1
 fi
 
--- setup_libs_linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-sudo apt install gcc-6 g++-6 libgmp-dev python-dev libboost-dev libcpprest-dev cmake git make zlib1g-dev libssl-dev libargon2-0-dev -y
-
-# fix for cpprest not finding xlocale
-sudo ln -s /usr/include/locale.h /usr/include/xlocale.h
-
