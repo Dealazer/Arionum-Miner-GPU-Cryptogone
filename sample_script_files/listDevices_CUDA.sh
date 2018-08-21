8262ccf5109d0154d9f2a4a89a622fd45f81c588
cryptogone <cryptogone.dev@gmail.com>
2018-08-21 16:55:55 +0200

    linux sample scripts (draft)

---
+++ sample_script_files/listDevices_CUDA.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+./arionum-cuda-miner.exe -l
+
+
---
+++ sample_script_files/run_CUDA.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# please change pool & wallet address to yours
+# adjust -b & -t value as described in the README and FAQ
+pool="http://aro.cool"
+wallet="4bb66RkoTCz63XPBvMyHfWRE1vWif21j77m1kNVNRd7o4YtJdEQWY7BsRVxAYoTdMexSVFGFaekrc3UATTSERwmQ"
+threads="1"
+batches="48"
+
+# -u means use all device, you can also use -d to specify list of devices (ex: -d 0,2,5)
+./arionum-opencl-miner -u -b "$batches" -t "$threads" -p "$pool" -a "$wallet"
+
