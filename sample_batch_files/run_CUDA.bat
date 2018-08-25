1332f42242e2ea67b2f98a9d1e25352dd22fb0d2
cryptogone <cryptogone.dev@gmail.com>
2018-08-25 12:26:28 +0200

    update samples with worker id

--- sample_batch_files/run_CUDA.bat
+++ sample_batch_files/run_CUDA.bat
@@ -1,13 +1,14 @@
 @echo OFF
 
-REM please change pool & wallet address to yours
+REM please change pool address, wallet address and worker ID to yours
 REM adjust -b & -t value as described in the README and FAQ
+set worker=""
 set pool="http://aro.cool"
 set wallet="4bb66RkoTCz63XPBvMyHfWRE1vWif21j77m1kNVNRd7o4YtJdEQWY7BsRVxAYoTdMexSVFGFaekrc3UATTSERwmQ"
 set threads="1"
 set batches="48"
 
 REM -u means use all device, you can also use -d to specify list of devices (ex: -d 0,2,5)
-arionum-cuda-miner.exe -u -b %batches% -t %threads% -p %pool% -a %wallet%
+arionum-cuda-miner.exe -u -b %batches% -t %threads% -p %pool% -a %wallet% -i %worker%
 
 pause
--- sample_script_files/run_CUDA.sh
+++ sample_script_files/run_CUDA.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
 
-# please change pool & wallet address to yours
+# please change pool address, wallet address and worker ID to yours
 # adjust -b & -t value as described in the README and FAQ
+worker=""
 pool="http://aro.cool"
 wallet="4bb66RkoTCz63XPBvMyHfWRE1vWif21j77m1kNVNRd7o4YtJdEQWY7BsRVxAYoTdMexSVFGFaekrc3UATTSERwmQ"
 threads="1"
 batches="48"
 
 # -u means use all device, you can also use -d to specify list of devices (ex: -d 0,2,5)
-./arionum-cuda-miner -u -b "$batches" -t "$threads" -p "$pool" -a "$wallet"
+./arionum-cuda-miner -u -b "$batches" -t "$threads" -p "$pool" -a "$wallet" -i $"worker"
 
