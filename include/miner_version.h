88eccffa17930be9f00c80d219f7cb298a1807aa
cryptogone <cryptogone.dev@gmail.com>
2018-09-20 11:10:52 +0200

    bump to v1.6

--- include/miner_version.h
+++ include/miner_version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-const std::string MINER_VERSION = "1.5.1";
+const std::string MINER_VERSION = "1.6.0";
 
 inline std::string getVersionStr() {
 	return std::string("Arionum GPU Miner version ") + MINER_VERSION;
