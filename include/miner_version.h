23bb58f49ad0d9b2463bb136d9ec415832277150
cryptogone <cryptogone.dev@gmail.com>
2018-11-11 14:26:23 +0100

    set version to 1.6 alpha for now

--- include/miner_version.h
+++ include/miner_version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-const std::string MINER_VERSION = "1.6.0";
+const std::string MINER_VERSION = "1.6.0-alpha";
 
 inline std::string getVersionStr() {
 	return std::string("Arionum GPU Miner version ") + MINER_VERSION;
