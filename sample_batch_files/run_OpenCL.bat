48859c8b8c5eb75ef09bb829ada0aef4eb81fb72
cryptogone <cryptogone.dev@gmail.com>
2018-11-22 19:20:53 +0100

    improve sample scripts, add stats node url

--- sample_batch_files/run_OpenCL.bat
+++ sample_batch_files/run_OpenCL.bat
@@ -2,13 +2,23 @@
 
 REM please change pool address, wallet address and worker ID to yours
 REM adjust -b & -t value as described in the README and FAQ
+REM -u means use all devices, you can also use -d to specify list of devices (ex: -d 0,2,5)
+set devices="-u"
 set worker=""
 set pool="http://aro.cool"
 set wallet="4bb66RkoTCz63XPBvMyHfWRE1vWif21j77m1kNVNRd7o4YtJdEQWY7BsRVxAYoTdMexSVFGFaekrc3UATTSERwmQ"
-set threads="2"
-set batches="24"
+set threads="1"
+set batches="64"
+set stats_node_url=""
+set stats_node_secret=""
 
-REM -u means use all device, you can also use -d to specify list of devices (ex: -d 0,2,5)
-arionum-opencl-miner.exe -u -b %batches% -t %threads% -p %pool% -a %wallet% -i %worker%
+REM set this to true to bench the miner performance
+set test_mode="false"
+set extraPrm=""
+if %test_mode% == "true" (
+	set extraPrm="--test-mode"
+)
+
+arionum-opencl-miner.exe %devices% %extraPrm% -b %batches% -t %threads% -p %pool% -a %wallet% -i %worker% -n %stats_node_url% -s %stats_node_secret%
 
 pause
--- sample_script_files/run_CUDA.sh
+++ sample_script_files/run_CUDA.sh
@@ -2,19 +2,28 @@
 
 # please change pool address, wallet address and worker ID to yours
 # adjust -b & -t value as described in the README and FAQ
+# -u means use all devices, you can also use -d to specify list of devices (ex: -d 0,2,5)
+devices="-u"
 worker=""
 pool="http://aro.cool"
 wallet="4bb66RkoTCz63XPBvMyHfWRE1vWif21j77m1kNVNRd7o4YtJdEQWY7BsRVxAYoTdMexSVFGFaekrc3UATTSERwmQ"
 threads="1"
-batches="48"
+batches="64"
+stats_node_url=""
+stats_node_secret=""
+test_mode="false" # set this to true to bench the miner performance
+
+extraPrm=""
+if [ "test_mode" = "true" ]; then
+    extraPrm="--test-mode"
+fi
 
 # set this to false if you do not want miner to auto relaunch after crash
 relaunch_miner_on_crash="true"
 
 while :
 do
-	# -u means use all device, you can also use -d to specify list of devices (ex: -d 0,2,5)
-	./arionum-cuda-miner -u -b "$batches" -t "$threads" -p "$pool" -a "$wallet" -i "$worker"
+	./arionum-cuda-miner "$devices" "$extraPrm" -b "$batches" -t "$threads" -p "$pool" -a "$wallet" -i "$worker" -n "$stats_node_url" -s "$stats_node_secret"
 
 	if [ "$relaunch_miner_on_crash" = "true" ]; then
 		echo "miner crashed, relaunching in 5 seconds ..."
