41a6daa6cead8dea97618f5fb83fc78886f05507
Dealazer <dealazerone@gmail.com>
2019-06-15 12:17:18 +0200

    Add files via upload

---
+++ build/argon2-gpu/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/src/v1.6-opt/argon2-gpu
+# Build directory: C:/src/v1.6-opt/build/argon2-gpu
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("ext/argon2")
---
+++ build/argon2-gpu/ext/argon2/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/src/v1.6-opt/argon2-gpu/ext/argon2
+# Build directory: C:/src/v1.6-opt/build/argon2-gpu/ext/argon2
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
